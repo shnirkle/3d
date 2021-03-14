c35876018275fc4b99c14970bdd684e4585878ad
Viola <peonie.pferd@gmail.com>
2021-03-14 20:23:07 +0100

    Pausemenü hinzugefügt

---
+++ src/renderer/Optionen.java
@@ -0,0 +1,112 @@
+package renderer;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+
+// Bereiche, die Optionen abdecken:
+// - Leistung / Qualität (FPS Limit, Anzahl der Objekte, View Distance, Orientierungshilfen)
+// - Raumschiff (FOV, Beschleunigung / max. Tempo)
+// - Audio (SFX, Musik)?
+// - Weiteres (Wireframe)
+
+public class Optionen {
+  
+  JFrame frame;
+  
+  //TabbedPane: Layout, das das Wechseln zwischen Panels mithilfe von Tabs erlaubt.
+  //Viel praktischer als das CardLayout (mit dem ich erstmal meine Zeit verschwendet habe -- ahhhh!!!), 
+  //da das Wechseln zwischen den Panels im Design inbegriffen ist --> keine extra Knöpfe
+  JTabbedPane tabpane;
+  
+  //Insgesamt habe ich vier Bereiche in den Optionen => 4 Panels
+  JPanel panelLeistung;
+  JPanel panelRaumschiff;
+  JPanel panelAudio;
+  JPanel panelWeiteres;
+ 
+  public Optionen(){
+	init();					//Initialisierung der Variablen (zusammengefasst in Funktion)
+	setupTabbedLayout();	//Implementierung des TabbedLayouts
+	specializePanels();		//Jedes große "Kategorie-Panel" enthält Subpanels, die wiederum GUI-Elemente für Einstellungen enthalten. Dafür hatte ich allerdings keine Zeit mehr
+    
+	frame.add(tabpane);	
+	
+	
+	
+	//----------------------- Frame Einstellungen ------------------------
+	frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //Frame wird beim Klick auf rotes Kreuz geschlossen, aber nicht das ganze Programm (dispose statt exit)
+	frame.setPreferredSize(new Dimension(900, 500));
+	frame.pack();
+	frame.setLocationRelativeTo(null); 
+	frame.setVisible(true);
+  }
+  //Methode, in der der ganze Initialisierungs-Mist drin ist - dann wird der Konstruktor nicht zugemüllt
+  public void init() {
+	  
+	  frame = new JFrame("Einstellungen");
+	  
+	  panelLeistung = new JPanel();
+	  panelRaumschiff = new JPanel();
+	  panelAudio = new JPanel();
+	  panelWeiteres = new JPanel();
+  }
+  
+  public void setupTabbedLayout() {
+
+	  tabpane = new JTabbedPane(JTabbedPane.TOP, JTabbedPane.SCROLL_TAB_LAYOUT);
+	  
+	  tabpane.addTab("Leistung", panelLeistung);
+	  tabpane.addTab("Raumschiff", panelRaumschiff);
+	  tabpane.addTab("Audio", panelAudio);
+	  tabpane.addTab("Weiteres", panelWeiteres);
+	  
+	  //------------ Verschönerung der Tab-Titel ---------------
+	  JLabel lab = new JLabel("Leistung");
+	  lab.setPreferredSize(new Dimension(50, 30));
+	  tabpane.setTabComponentAt(0, lab);  // tab index, jLabel
+  }
+  
+  public void specializePanels() {
+	  //Diese Panels dienen momentan nur als Platzhalter, da mir die Zeit fehlte, die Optionen fertig zu machen.
+	  //Es können noch mehr Subpanels in den Panels (Leistung, Raumschiff, etc.) erschaffen und nach einem beliebigen Layout angeordnet werden.
+	  //Letztendlich würden die Subpanels die für den Einstellungsbereich spezifischen Einstellungen beinhalten.
+	  //Bei Raumschiff könnte man zum Beispiel die Geschwindigkeit einstellen, bei Leistung die maximalen FPS und so weiter.
+	  //Je nach Einstellung kann man dafür Slider, Textfelder, usw. benutzen.
+	  
+	  JPanel leistung1 = new JPanel();
+	  JPanel raumschiff1 = new JPanel();
+	  JPanel audio1 = new JPanel();
+	  JPanel weiteres1 = new JPanel();
+	  
+	  panelLeistung.add(leistung1);
+	  panelRaumschiff.add(raumschiff1);
+	  panelAudio.add(audio1);
+	  panelWeiteres.add(weiteres1);
+	  
+	  //---------- Hinzufügen von GUI-Elementen auf den Subpanels - erstmal nur JLabels als Platzhalter ----------
+	  JLabel labLeistung = new JLabel("Vorstellbare Einstellungen: Maximale FPS, Renderdistanz der Objekte, maximale Anzahl an Objekten, ...");
+	  JLabel labRaumschiff = new JLabel("Vorstellbare Einstellungen: Geschwindigkeit, FOV, Steuerung (+ Anzeigen der momentanen Tastenbelegung), ...");
+	  JLabel labAudio = new JLabel("Vorstellbare Einstellungen (hätten wir Musik gemacht): Musiklautstärke, SFX-Lautstärke, ...");
+	  JLabel labWeiteres = new JLabel("Vorstellbare Einstellungen: Wireframe (nur Kanten der Objekte), andere Einstellungen ohne eindeutige Kategorie");
+	  
+	  leistung1.add(labLeistung);
+	  raumschiff1.add(labRaumschiff);
+	  audio1.add(labAudio);
+	  weiteres1.add(labWeiteres);
+	  
+//	//----------------------- Ästhetik -----------------------
+//		Color backgroundColor = new Color(48, 52, 64);
+//		leistung1.setBackground(backgroundColor);
+//		raumschiff1.setBackground(backgroundColor);
+//		audio1.setBackground(backgroundColor);
+//		weiteres1.setBackground(backgroundColor);
+  }
+ 
+}
+
---
+++ src/renderer/PauseView.java
@@ -0,0 +1,191 @@
+package renderer;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+public final class PauseView  implements ActionListener{
+	
+	JFrame frame;
+	JLabel label;
+	
+	JButton buttonWeiter;
+	JButton buttonOptionen;
+	JButton buttonVerlassen;
+	
+	//5 Panels (Nord, Süd, West, Ost, Mitte). Letztendlich wird nur das in der Mitte genutzt. Die anderen dienen dazu, einen Rand zu lassen
+	JPanel panel1;
+	JPanel panel2;
+	JPanel panel3;
+	JPanel panel4;
+	JPanel panel5;	//Panel 5 wird später das zentrale Panel. Es enthält die Knöpfe.
+	
+	//3 Panels, die sich in Panel 5 befinden
+	JPanel panel6;
+	JPanel panel7;
+	JPanel panel8;
+	
+	public PauseView() {
+		
+		label = new JLabel("-  Pausiert  -");
+		
+		buttonWeiter = new JButton ("Weiter");
+		buttonOptionen = new JButton("Einstellungen");
+		buttonVerlassen = new JButton("Spiel verlassen");
+		/*
+		buttonWeiter.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+		        frame.dispose();
+		    }
+		});*/
+		
+		frame = new JFrame();
+		frame.setLayout(new BorderLayout());
+		
+		panel1 = new JPanel();
+		panel2 = new JPanel();
+		panel3 = new JPanel();
+		panel4 = new JPanel();
+		panel5 = new JPanel();
+		
+		//Farben können natürlich auch verändert werden, um Panels zu visualisieren
+		Color backgroundColor = new Color(48, 52, 64);
+		panel1.setBackground(backgroundColor);
+		panel2.setBackground(backgroundColor);
+		panel3.setBackground(backgroundColor);
+		panel4.setBackground(backgroundColor);
+		panel5.setBackground(backgroundColor);
+		
+		//Breite und Höhe (in der Reihenfolge) der Dimensionen der Panels
+		panel1.setPreferredSize(new Dimension(100,100)); //Dimension(Breite, Höhe)
+		panel2.setPreferredSize(new Dimension(70,100));
+		panel3.setPreferredSize(new Dimension(100,100));
+		panel4.setPreferredSize(new Dimension(75,100));
+		panel5.setPreferredSize(new Dimension(100,60));
+		
+		//Panels mit dem BorderLayout haben fünf Ausrichtungen: oben, rechts, unten, links, Mitte
+		frame.add(panel1, BorderLayout.NORTH);
+		frame.add(panel2, BorderLayout.EAST);
+		//frame.add(panel3, BorderLayout.SOUTH);
+		frame.add(panel4, BorderLayout.WEST);
+		frame.add(panel5, BorderLayout.CENTER);
+		
+		//----------------------- Panels innerhalb des zentralen Panels (panel5): Sub Panels ------------------
+		panel6 = new JPanel();
+		panel7 = new JPanel();
+		panel8 = new JPanel();
+
+		panel6.setBackground(backgroundColor);
+		panel7.setBackground(backgroundColor);
+		panel8.setBackground(backgroundColor);
+		
+		panel6.setPreferredSize(new Dimension(50,50)); 
+		panel7.setPreferredSize(new Dimension(50,50));
+		panel8.setPreferredSize(new Dimension(50,50));
+
+		panel5.setLayout(new GridLayout(4,1, 100,10)); 	//GridLayout(Zeilen, Spalten der Tabelle, Abstand rechts / links, Abstand unten / oben)
+		panel5.add(panel6, BorderLayout.NORTH); 
+		panel5.add(panel7, BorderLayout.EAST);
+		panel5.add(panel8, BorderLayout.SOUTH);
+
+		//----------------------- Buttons auf den drei Panels: Weiter (unpausieren), Optionen (neues Fenster, Optionen), Spiel verlassen -----------------------
+		panel6.add(buttonWeiter);
+		panel7.add(buttonOptionen);
+		panel8.add(buttonVerlassen);
+			
+		buttonWeiter.addActionListener(this);
+		buttonOptionen.addActionListener(this);
+		buttonVerlassen.addActionListener(this);
+		
+		buttonWeiter.setFocusable(false);		//Option lässt das Rechteck, das in einem Button erscheint, wenn man ihn angeklickt hat, verschwinden
+		buttonOptionen.setFocusable(false);
+		buttonVerlassen.setFocusable(false);
+		
+		buttonWeiter.setBackground(backgroundColor);
+		buttonOptionen.setBackground(backgroundColor);
+		buttonVerlassen.setBackground(backgroundColor);
+		
+		buttonWeiter.setForeground(Color.white);
+		buttonOptionen.setForeground(Color.white);
+		buttonVerlassen.setForeground(Color.white);
+		
+		buttonWeiter.setFont(new Font("Calibri", Font.BOLD ,20));
+		buttonOptionen.setFont(new Font("Calibri", Font.BOLD ,20));
+		buttonVerlassen.setFont(new Font("Calibri", Font.BOLD ,20));;
+		
+		//----------------------- "Pausiert" Label -----------------------
+		panel1.add(label);
+		label.setForeground(Color.white);
+		label.setFont(new Font("Calibri", Font.BOLD ,25));
+		label.setBorder(BorderFactory.createEmptyBorder(20, 10, 10, 10));	//Rand
+		
+		//----------------------- Frame Einstellungen -----------------------------------
+		//frame.addActionListener(this); eigentlich will ich einen Listener für den ganzen Frame. mal schauen, wie das geht. mach ich aber ein andermal
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);	//Frame wird beim Klick auf rotes Kreuz geschlossen, aber nicht das ganze Programm (dispose statt exit)
+		//frame.setTitle("Pause");
+		frame.setPreferredSize(new Dimension(400, 500));
+		frame.setUndecorated(true);			//versteckt die Titelleiste. Muss vor pack() geschrieben werden.
+		frame.pack();
+		frame.setLocationRelativeTo(null);	//zentriert das Fenster auf dem Bildschirm
+		frame.setVisible(true);
+	    //frame.setResizable(false);			//Fenster kann nicht vergrößert / verkleinert werden
+	    
+	}
+	
+//	//ein ActionListener für alle Buttons
+//	void addPauseListener(ActionListener listenPauseBtn) {
+//		buttonWeiter.addActionListener(listenPauseBtn);
+//		buttonOptionen.addActionListener(listenPauseBtn);
+//		buttonVerlassen.addActionListener(listenPauseBtn);
+//	}
+//	
+//	
+//	
+//	//Funktion wird gebraucht, um den Frame in der Controllerklasse schließen zu können (Klick auf Weiter)
+//	public static JFrame getPauseFrame() {
+//		return PauseView.frame;
+//	}
+//	
+//	void closeFrame() {
+//		frame.dispose();
+//	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		//Button: Weiter
+		if(e.getSource()==buttonWeiter) {
+			frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));	//schließt das PauseScreen Fenster
+		}
+		
+		//Button: Optionen
+		if(e.getSource()==buttonOptionen) {
+			//schließt aktuelles Fenster, um Optionen zu öffnen	
+			frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING)); 
+			Optionen optionen = new Optionen();	
+		}
+		
+		//Button: Spiel verlassen
+		if(e.getSource()==buttonVerlassen) {
+			System.exit(0);	//Beendet das Programm. Ein Wert, der nicht null wäre, würde angeben, dass ein Fehler der Grund für das Schließen des Programms war
+		}
+		
+	}
+	
+}
+
